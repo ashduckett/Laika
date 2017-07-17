91ea6e5414b7bb5aa4bbad1da2206439937123c7
Ash Duckett <ashduckett@Ashs-iMac.local>
2017-07-17 09:25:34 +0100

    Clear out template code, nothing but a black screen and an appropriately sized scene is left.

--- Laika/GameViewController.swift
+++ Laika/GameViewController.swift
@@ -14,22 +14,16 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scene = GameScene(size: CGSize(width: 1536, height: 2048))
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+        
     }
 
     override var shouldAutorotate: Bool {
