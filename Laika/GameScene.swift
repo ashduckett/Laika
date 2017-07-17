192b42cd76dcff2cd8df57c5d62bede71b061ae8
Ash Duckett <ashduckett@Ashs-iMac.local>
2017-07-17 10:28:03 +0100

    Add debug rect so I can see the area the user will see regardless of device.

--- Laika/GameScene.swift
+++ Laika/GameScene.swift
@@ -10,12 +10,40 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var playableRect: CGRect
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    
+    func debugDrawPlayableArea() {
+        let shape = SKShapeNode()
+        let path = CGMutablePath()
+        path.addRect(playableRect)
+        shape.path = path
+        shape.strokeColor = SKColor.green
+        shape.lineWidth = 4.0
+        addChild(shape)
+    }
+    
+    
+    override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = 16.0 / 9.0
+        let playableWidth = size.height / maxAspectRatio
+        let playableMargin = (size.width - playableWidth) / 2.0
+        playableRect = CGRect(x: playableMargin, y: 0, width: playableWidth, height: size.height)
+        
+        
+        print("Width is \(playableRect.width)")
+        
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        // debugDrawPlayableArea()
     }
     
     
